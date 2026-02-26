63a7626ab83f85f8148025b57985bfe5815541ed
turbatdd <49607815+turbatdd@users.noreply.github.com>
2026-02-25 22:39:26 -0600

    Prob2 Donoe

--- src/Asignment2_Prob2/Prob2.java
+++ src/Asignment2_Prob2/Prob2.java
@@ -11,45 +11,81 @@ public class Prob2 {
 
     void main() {
         String[] sampleZone = {"America/Panama", "America/Chicago", "America/Indiana/Indianapolis", "America/Santiago", "America/Phoenix"};
-
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm");
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter your event name");
-        String name = sc.next();
-
-        System.out.println("Enter your event Date /Eg.2002-01-22/");
-        String date = sc.next();
-
-        System.out.println("Enter your event Time /Eg.01:33/");
-        String dateTime = sc.next();
-        String dateStr = date + "T" + dateTime + "Z";
-
-        dateStr = "2026-05-02T03:40";
-
-        LocalDate rawDate = LocalDate.parse(dateStr, dtf);
-
-
-        LocalDate theDate = LocalDate.parse(dateStr, dtf);
-        Period pr = Period.between(LocalDate.now(), theDate);
-
-        System.out.println("Day of Week: " + theDate.getDayOfWeek());
-        System.out.println("Is Leap Year: " + theDate.isLeapYear());
-        System.out.println("Event date and now Date: " + pr.getYears() + " years, " + pr.getMonths() + " months, " + pr.getDays() + " days");
-
-        ZoneId currentZone = ZoneId.systemDefault();
-        System.out.println("Your current timezone: " + currentZone.toString());
-        System.out.println("Select Your zone by Index\n" + Arrays.toString(sampleZone));
-
-        int zoneIndex = sc.nextInt();
-
         DateTimeFormatter dtfNice = DateTimeFormatter.ofPattern("EEEE, MMMM dd, yyyy @ HH:mm [VV]");
 
 
-//        String formatted = theDate.atZone(sampleZone[zoneIndex]).format(dtfNice);
+        Scanner sc = new Scanner(System.in);
 
-//        System.out.println(formatted);
+        try {
+            System.out.println("Enter your event name");
+            String name = sc.nextLine();
 
+            System.out.println("Enter your event Date /Eg.2002-01-22/");
+            String date = sc.nextLine();
+
+            System.out.println("Enter your event Time /Eg.01:33/");
+            String dateTime = sc.nextLine();
+            String dateStr = date + "T" + dateTime;
+
+//        dateStr = "2026-05-02T03:40";
+
+            LocalDateTime localDateTime = LocalDateTime.parse(dateStr, dtf);
+            LocalDate theDate = localDateTime.toLocalDate();
+            Period pr = Period.between(LocalDate.now(), theDate);
+
+            ZoneId currentZone = ZoneId.systemDefault();
+            ZonedDateTime zonedDate = localDateTime.atZone(currentZone);
+            ZonedDateTime curZonedDate = LocalDateTime.now().atZone(currentZone);
+
+            String strDateReport = getDateReport(name, currentZone.toString(), zonedDate.format(dtfNice), theDate.getDayOfWeek().toString(), theDate.isLeapYear(), curZonedDate.format(dtfNice), pr.getYears(), pr.getMonths(), pr.getDays());
+
+            System.out.println(strDateReport);
+
+            boolean isFinish = false;
+
+            do {
+                System.out.println("Do you want to change timezone Y/N ?");
+                String yesOrNo = sc.next().toLowerCase();
+
+                if (yesOrNo.equals("y")) {
+                    for (int i = 0; i < sampleZone.length; i++) {
+                        System.out.println("\n" + (i + 1) + ". " + sampleZone[i]);
+                    }
+                    System.out.println("Please enter your zone number");
+                    int zoneIndex = sc.nextInt();
+
+                    System.out.println("Your selected timezone is: " + sampleZone[zoneIndex - 1]);
+
+                    ZoneId selectedZone = ZoneId.of(sampleZone[zoneIndex - 1]);
+                    ZonedDateTime converted = zonedDate.withZoneSameInstant(selectedZone);
+
+                    System.out.println("Selected timezone is: " + selectedZone);
+                    System.out.println("Selected timezone Date is: " + converted.format(dtfNice));
+                } else {
+                    isFinish = true;
+                    System.out.println("Thank you see you later");
+
+                    break;
+                }
+            } while (!isFinish);
+        } catch (Exception e) {
+            System.out.println("Datetime is incorrect.");
+        }
+
+
+    }
+
+    public String getDateReport(String name, String timeZone, String date, String dayOfWeek, boolean isLeap, String nowDate, int difYears, int difMonths, int difDays) {
+        return String.format("""
+                Event name: %s
+                Your current timezone: %s
+                Your event date: %s
+                Day of week: %s
+                Is leap year: %s
+                Now date: %s
+                Now date and event date difference: %d-Years %d-Months %d-Days
+                """, name, timeZone, date, dayOfWeek, isLeap, nowDate, difYears, difMonths, difDays);
 
     }
 
