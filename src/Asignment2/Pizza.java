37cf032f7cbe4b44a128fd664ff96f4e66eef159
turbatdd <49607815+turbatdd@users.noreply.github.com>
2026-02-26 08:51:48 -0600

    Prob4 Done

--- src/Asignment2/Pizza.java
+++ src/Asignment2/Pizza.java
@@ -1,4 +1,58 @@
 package Asignment2;
 
+enum PizzaSize {
+    SMALL, MEDIUM, LARGE
+}
+
+enum PizzaType {
+    VEGGIE, PEPPERONI, CHEEZE, BBQCHICKEN
+}
+
 public class Pizza {
+
+
+    private final PizzaSize pizzaSize;
+    private final PizzaType pizzaType;
+    private final int quantity;
+    private double price;
+
+    public Pizza(PizzaSize pizzaSize, PizzaType pizzaType, int quantity) {
+        this.pizzaSize = pizzaSize;
+        this.pizzaType = pizzaType;
+        this.quantity = quantity;
+        this.price = calculatePrice(); // calculated internally
+    }
+
+    private double calculatePrice() {
+        double rawPrice = 0.0;
+
+        switch (pizzaSize) {
+            case SMALL -> rawPrice = 8.0;
+            case MEDIUM -> rawPrice = 10.0;
+            case LARGE -> rawPrice = 12.0;
+        }
+
+        switch (pizzaType) {
+            case VEGGIE -> rawPrice += 1;
+            case PEPPERONI -> rawPrice += 2;
+            case CHEEZE -> rawPrice += 1.5;
+            case BBQCHICKEN -> rawPrice += 2;
+        }
+
+        return rawPrice * quantity;
+    }
+
+    public String printOrderSummary() {
+        double taxPrice = price * 0.03;
+        return String.format(
+                """
+                        Pizza Order:
+                        Size: %s
+                        Type: %s
+                        Qty: %d
+                        Price: $%.2f
+                        Tax: $%.2f
+                        Total Price: $%.2f
+                        """, pizzaSize, pizzaType, quantity, price, taxPrice, price + taxPrice);
+    }
 }
--- src/Asignment2/Prob4.java
+++ src/Asignment2/Prob4.java
@@ -3,7 +3,11 @@ package Asignment2;
 public class Prob4 {
 
     public static void main(String[] args) {
-
+        Pizza pizza1 = new Pizza(PizzaSize.SMALL, PizzaType.VEGGIE, 2);
+        Pizza pizza2 = new Pizza(PizzaSize.MEDIUM, PizzaType.PEPPERONI, 1);
+        Pizza pizza3 = new Pizza(PizzaSize.LARGE, PizzaType.BBQCHICKEN, 2);
+        System.out.println(pizza1.printOrderSummary());
+        System.out.println(pizza2.printOrderSummary());
+        System.out.println(pizza3.printOrderSummary());
     }
-
 }
