1a30880361c93f18ff83a7d09c8bae26e1b2e323
turbatdd <49607815+turbatdd@users.noreply.github.com>
2026-03-10 14:44:42 -0500

    Prob 1 Fixed

---
+++ src/assignmentLesson8/prob3/StudentComparator.java
@@ -0,0 +1,17 @@
+package assignmentLesson8.prob3;
+
+import lesson8.Student;
+
+import java.util.Comparator;
+
+public class StudentComparator implements Comparator<Student> {
+    @Override
+    public int compare(Student o1, Student o2) {
+        if (o1.getName().compareTo(o2.getName()) != 0)
+            return o1.getName().compareTo(o2.getName());
+        else if (Double.compare(o1.getGpa(), o2.getGpa()) != 0)
+            return Double.compare(o1.getGpa(), o2.getGpa());
+        else
+            return Integer.compare(o1.getId(), o2.getId());
+    }
+}
